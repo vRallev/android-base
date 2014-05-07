3070dbe22244e3cb378a0c97c4e0ef4c6b7b07d0
Ralf Wondratschek <rwo@adorsys.de>
2014-05-07 17:22:20 +0200

    Add android-base 1.6.1

--- Android-Base-Project/android-base/src/main/java/net/vrallev/android/base/RetainObject.java
+++ Android-Base-Project/android-base/src/main/java/net/vrallev/android/base/RetainObject.java
@@ -3,12 +3,9 @@ package net.vrallev.android.base;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-import javax.inject.Qualifier;
-
 /**
  * @author Ralf Wondratschek
  */
-@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RetainObject {
 }
